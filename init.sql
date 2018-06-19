0088e81cd736445a6c90b37d9f9d53b0dea3eac9
0GGmr0 <18817619557@163.com>
2018-06-20 00:31:00 +0800

    更新init.sql文件

--- init.sql
+++ init.sql
@@ -1,13 +1,17 @@
 -- auto-generated definition
 create table user
 (
-  user_id  int         not null
+  user_id         varchar(16)     not null
     primary key,
-  password varchar(64) not null,
-  type     int         null
-  comment '1是计票员 2是管理员'
+  password        varchar(64)     not null,
+  type            int             null
+  comment '1是计票员 2是管理员',
+  party_count_num int default '0' null,
+  group_count_num int default '0' null
 )
   charset = utf8;
+
+
 
 create table party_candidate(
   party_candidate_name varchar(16) not null primary key ,
